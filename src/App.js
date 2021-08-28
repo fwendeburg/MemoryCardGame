5ada24c7634bf926ec011532917272b4187d9fe0
fwendeburg <86864927+fwendeburg@users.noreply.github.com>
2021-08-28 18:08:10 +0200

    installed styled-components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return ();
+  return (<p>Test</p>);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import { ResetCSS } from './global_styles/ResetCSS.style.js';
+import { BodyAndRootStyles } from './global_styles/BodyAndRoot.style.js';
+
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ResetCSS />
+    <BodyAndRootStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
